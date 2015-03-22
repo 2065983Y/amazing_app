11619d4c9c58637f94c8bf1b1378a29113a17e95
2064240P <2064240P@student.gla.ac.uk>
2015-03-22 18:44:37 +0000

    Maze Creating stuff

--- amazingapp/models.py
+++ amazingapp/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Maze(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    solved_by = models.TextField()
     rows = models.IntegerField()
     cols = models.IntegerField()
     cells = models.TextField()
+    solved_by = models.TextField()
     attempts = models.IntegerField(default = 0)
 #    creator = models.ForeignKey(User)
     __grid = None
