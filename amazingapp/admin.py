eccdd2a197f4743ba147c188c44f3607ea50f862
2096009 <hyfolt@gmail.com>
2015-03-22 21:30:03 +0000

    Population script(beta), user/profiles

--- amazingapp/admin.py
+++ amazingapp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from amazingapp.models import Maze
+from amazingapp.models import Maze, UserProfile
+
+class MazeAdmin(admin.ModelAdmin):
+	list_display = ('name', 'solved_by', 'rows', 'cols', 'cells', 'attempts')
 # Register your models here.
 
 admin.site.register(Maze)
+admin.site.register(UserProfile)
