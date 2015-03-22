57048abe1246e7436596d7799393b929b53ce111
2096009 <hyfolt@gmail.com>
2015-03-22 00:04:00 +0000

    Home page

--- amazingapp/urls.py
+++ amazingapp/urls.py
@@ -4,4 +4,7 @@ from amazingapp import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+	url(r'^builders/', views.builders, name='builders'),
+	url(r'^solvers/', views.solvers, name='solvers'),
+	url(r'^mazes/', views.mazes, name='mazes'),
 )
--- amazingapp/views.py
+++ amazingapp/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-
+from amazingapp.models import Maze, UserProfile
 # Create your views here.
 
 def index(request):
-    
-    return render(request, 'base.html', {})
-    
+
+    mules_list = UserProfile.objects.order_by('-mazes_created')[:5]
+    cats_list = UserProfile.objects.order_by('-mazes_solved')[:5]
+
+    context_dict = {'mules': mules_list, 'cats': cats_list}
+
+
+    response = render(request,'index.html', context_dict)
+
+    return response
+	
+def builders(request):
+	return render(request,'index.html', {})
+
+def solvers(request):
+	return render(request,'index.html', {})
+	
+def mazes(request):
+	return render(request,'index.html', {})
