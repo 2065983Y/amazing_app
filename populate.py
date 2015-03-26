b19ea19d4ac12b7c873d5ef56d7b49f3d0453562
2096009 <hyfolt@gmail.com>
2015-03-26 21:36:51 +0000

    Population script - working

--- populate.py
+++ populate.py
@@ -11,36 +11,58 @@ django.setup()
 from amazingapp.models import Maze, UserProfile
 from django.contrib.auth.models import User
 
+def add_user(username, email, password):
+	try:
+		u = User.objects.get(username=username)
+	except:
+		u = User.objects.create_user(username, email, password)
+	return u
 
+def add_user_profile(user, mazes_created, mazes_solved):
+	p = UserProfile.objects.get_or_create(user=user, mazes_created=mazes_created, mazes_solved=mazes_solved)[0]
+	return p
+
+def add_maze(name, rows, cols, cells, attempts, solved, creator):
+	maze = Maze.objects.get_or_create(name=name, rows=rows, cols=cols, cells=cells,
+                                      attempts=attempts, solved = solved, creator = creator)[0]
+	return maze
+	
 def populate():
-    add_maze("Destroyer583", 4, 4, "1000110001100011", 2)
 
-    add_maze("Daedalus", 4, 4, "1000111111100011", 2)
+	# Users
+	fox = add_user('fox', 'fox@student.gla.ac.uk', 'fox')
+	fox.save()
+	dog = add_user('dog', 'dog@student.gla.ac.uk', 'dog')
+	dog.save()
+	test = add_user('test', 'test@student.gla.ac.uk', 'test')
+	test.save()
+	
+	# User profiles
+	Fox = add_user_profile(fox, 5, 5)
+	Fox.save()
+	Dog = add_user_profile(dog, 4, 8)
+	Dog.save()
+	Test = add_user_profile(test, 12, 3)
+	Test.save()
+	
+	test1 = add_maze("Test1", 4, 4, "1000110001100011", 2,  True, fox)
+	test1.solved_by.add(dog)
+	test1.save()
+	de = add_maze("Destroyer583", 4, 4, "1000110001100011", 2,  True, fox)
+	de.solved_by.add(test)
+	de.save()
+	labas = add_maze("Labas", 4, 4, "1000110001101011", 2,  True, dog)
+	labas.solved_by.add(fox)
+	labas.save()
+	lab = add_maze("Lab", 4, 4, "101011001100011", 2,  True, fox)
+	lab.solved_by.add(test)
+	lab.save()
+	circus = add_maze("Circus", 4, 4, "1111100111000110001100011", 2,  True, test)
+	circus.solved_by.add(fox)
+	circus.save()
 
-    add_maze("John", 5, 5, "1111111111000110001100011", 2)
 
-    add_maze("Jim", 5, 5, "1111100111000110001100011", 2)
-
-    add_profile("a", "l@l.com", "1", 100, 50)
-    add_profile("b", "l@l.com", "1", 14, 12)
-    add_profile("c", "l@l.com", "1", 1100, 100)
-    add_profile("d", "l@l.com", "1", 180, 100)
-    add_profile("e", "l@l.com", "1", 10, 10)
-
-
-def add_maze(name, rows, cols, cells, attempts, solved_by="John"):
-    maze = Maze.objects.get_or_create(name=name, rows=rows, cols=cols, cells=cells, solved_by=solved_by,
-                                      attempts=attempts)
-    return maze
-
-
-def add_profile(username, email, password, created, solved):
-    user = User.objects.create_user(username=username, email=email, password=password)
-    profile = UserProfile.objects.get_or_create(user=user, email=email, password=password, mazes_created=created,
-                                                mazes_solved=solved)
-    return profile
-
-
+	
 if __name__ == '__main__':
     print "Starting population script..."
     populate()
